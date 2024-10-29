0fb77282790fe1f5cf5d9a81a2a1da0b3f5892b7
nomercyforkittyz <zavarykingleb@gmail.com>
2024-10-29 22:53:05 +0400

    add task-2

--- src/task_2/Main.java
+++ src/task_2/Main.java
@@ -1,5 +1,13 @@
 package task_2;
 
-public class Main {
+import task_2.validator.UserValidator;
 
+public class Main {
+    public static void main(String[] args) {
+        boolean isValid = UserValidator.validateUser("login_123", "pass111", "pass123");
+
+        if (isValid){
+            System.out.println("All is valid");
+        }
+    }
 }
